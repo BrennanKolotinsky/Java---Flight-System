13accca4cadf4fe73afc4a4706d4f57c6f4a72e7
Brennan Kolotinsky <BrennanKolotinsky@gmail.com>
2020-01-23 01:04:57 -0500

    refactored code into to parts, setup the JSON parser

--- src/Test.java
+++ src/Test.java
@@ -1,7 +1,5 @@
 import java.util.ArrayList;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Iterator;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -10,10 +8,14 @@ import org.json.simple.parser.ParseException;
 
 public class Test {
 	
-	// let's build the project out of our flights class
-	public static void main(String[] args) {
-		
-		// PART 1:
+	// let's build the project out of our flights class -- add those throws to skip adding try/catch to our parser
+	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+		createFlightsPart1();
+		createFlightsPart2();
+	}
+	
+	// PART 1:
+	public static void createFlightsPart1() {
 		// let's create our flight objects
 		Flight aFlight1 = new Flight(1, "YUL", "YYZ");
 		Flight aFlight2 = new Flight(1, "YUL", "YYC");
@@ -34,11 +36,13 @@ public class Test {
 		// pass in our array list of flights and construct a flights class
 		Flights flights1 = new Flights(FlightsToAdd);
 		flights1.printAllFlights();
-		
-		// PART 2:
+	}
+	
+	// PART 2:
+	public static void createFlightsPart2() throws FileNotFoundException, IOException, ParseException {
+		// code based off https://stackoverflow.com/questions/10926353/how-to-read-json-file-into-java-with-simple-json-library --> note I threw the JSON in an array for simplicity
 		JSONParser parser = new JSONParser();
-		JSONArray a = (JSONArray) parser.parse(new FileReader("/Users/brennan/eclipse-workspace/Java/FlightSystem/coding-assigment-orders.json"));
-		
-		
+		JSONArray a = (JSONArray) parser.parse(new FileReader("/Users/brennan/eclipse-workspace/Java/FlightSystem/src/coding-assigment-orders.json"));
+
 	}
 }
